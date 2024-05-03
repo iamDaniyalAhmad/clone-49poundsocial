08510a18c90a299894ba808f6021030d574e2ee9
iamDaniyalAhmad <SP22BSCS0087@maju.edu.pk>
2024-05-03 18:11:32 +0500

    Changes in Tailwind Css

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     };
 
     return (
-        <div className="navbar-section bg-[#024963]">
+        <div className=" bg-[#024963]">
             <div className="navbar-content flex justify-between items-center py-8">
                 <div className="logo">
                     <img src="images/49-pound-social-logo-yellow-and-white.webp" alt="" className="ml-10 w-36 h-12" />
@@ -21,20 +21,21 @@ const Navbar = () => {
                     <span className="menu-icon"></span>
                 </button>
                 {/* Navbar menu */}
+                
                 <div className={`navbar-menu md:block ${isMobileMenuOpen? 'block' : 'hidden'}`}>
-                    <ul className="flex">
+                    <ul className="md:flex">
                         <li className="mr-10 text-white">OUR STORY</li>
                         <li className="mr-10 text-white nav-item dropdown">
                             <a className="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 WHAT WE DO
                             </a>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Social Media Management</a></li>
-                                <li><a className="dropdown-item" href="#">Social Media Marketing</a></li>
-                                <li><a className="dropdown-item" href="#">SEO Blogs</a></li>
-                                <li><a className="dropdown-item" href="#">Branding & Design</a></li>
-                                <li><a className="dropdown-item" href="#">Resell</a></li>
-                                <li><a className="dropdown-item" href="#">Referral Scheme</a></li>
+                                <li className='mt-3'><a className="dropdown-item" href="#">Social Media Management</a></li>
+                                <li className='mt-3'><a className="dropdown-item" href="#">Social Media Marketing</a></li>
+                                <li className='mt-3'><a className="dropdown-item" href="#">SEO Blogs</a></li>
+                                <li className='mt-3'><a className="dropdown-item" href="#">Branding & Design</a></li>
+                                <li className='mt-3'><a className="dropdown-item" href="#">Resell</a></li>
+                                <li className='mt-3'><a className="dropdown-item" href="#">Referral Scheme</a></li>
                             </ul>
                         </li>
                         <li className="mr-10 text-white">CONTACT</li>
